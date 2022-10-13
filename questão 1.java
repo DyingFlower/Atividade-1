d721cae15e75a1b2331e2c23c8b493575ce17de5
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-10-13 10:58:59 -0300

    Create questão 1.java

---
+++ questão 1.java
@@ -0,0 +1,19 @@
+package atividadeplay;
+
+public interface IPlay {
+	public void start();
+
+	public void stop();
+
+	public void next(int quantidade);
+
+	public void back(int quantidade);
+
+	public void pause();
+	
+	public String fabricante();
+
+	
+}
+
+
